10e6ff1b0890542ded7ab478c68cfc631da7f797
petercolella <peter.joseph.colella@gmail.com>
2019-06-16 22:38:25 -0400

    about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,76 @@
 import React from 'react';
+import './style.css';
 
-const About = props => <h1>About</h1>;
+const About = () => (
+  <>
+    <div className="hero-image mb-3">
+      <div className="hero-text">
+        <h1>Pupster</h1>
+        <h2>Like Tinder for Dogs</h2>
+        <h3>...but no weird stuff</h3>
+      </div>
+    </div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Welcome to Pupster</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <p>
+            Doggo ipsum fat boi pats ur givin me a spook very jealous pupper,
+            mlem most angery pupper I have ever seen. Boofers heckin angery
+            woofer yapper maximum borkdrive adorable doggo, tungg he made many
+            woofs. You are doin me a concern heck blep doggorino long bois
+            puggo, many pats very hand that feed shibe thicc. What a nice floof
+            puggo wow such tempt bork, long water shoob. Big ol pupper aqua
+            doggo noodle horse very good spot most angery pupper I have ever
+            seen, boofers bork very good spot. Adorable doggo long woofer ur
+            givin me a spook big ol smol such treat you are doing me the shock,
+            shoober floofs heckin ur givin me a spook vvv very hand that feed
+            shibe, doggorino long doggo ruff dat tungg tho you are doing me a
+            frighten.
+          </p>
+          <p>
+            Big ol you are doing me a frighten puggorino pupperino smol borking
+            doggo with a long snoot for pats thicc, doing me a frighten you are
+            doin me a concern puggorino. Pupper bork ur givin me a spook clouds
+            maximum borkdrive pupper, lotsa pats wow such tempt very hand that
+            feed shibe. Corgo heck wrinkler doggo most angery pupper I have ever
+            seen, doggorino very jealous pupper the neighborhood pupper. Doing
+            me a frighten ruff doing me a frighten bork you are doin me a
+            concern, shoob what a nice floof borkf. Very good spot shibe doing
+            me a frighten fluffer much ruin diet waggy wags, borkdrive heck aqua
+            doggo. Shooberino very taste wow noodle horse blep noodle horse
+            clouds, long bois snoot much ruin diet. Shoober long water shoob
+            stop it fren very good spot, big ol wow such tempt. Very jealous
+            pupper borkdrive woofer puggorino much ruin diet, blep long woofer
+            heckin, he made many woofs maximum borkdrive much ruin diet. He made
+            many woofs fat boi borking doggo shooberino heckin good boys and
+            girls length boy, very good spot very hand that feed shibe very
+            taste wow maximum borkdrive. Very hand that feed shibe thicc blop,
+            boofers.
+          </p>
+          <p>
+            Ruff tungg clouds extremely cuuuuuute boof, heck sub woofer
+            pupperino. He made many woofs heckin good boys and girls adorable
+            doggo, borking doggo. Long water shoob shooberino I am bekom fat
+            heckin good boys and girls lotsa pats pupperino, wrinkler shoober
+            tungg. Long bois heckin good boys and girls dat tungg tho mlem corgo
+            maximum borkdrive, very taste wow clouds thicc. Long woofer super
+            chub ruff floofs pupperino doggorino, heckin angery woofer length
+            boy heckin good boys and girls he made many woofs, porgo heck ur
+            givin me a spook length boy. Ruff very jealous pupper big ol pupper
+            many pats stop it fren puggo sub woofer, borking doggo long water
+            shoob bork snoot the neighborhood pupper. Porgo thicc blep lotsa
+            pats, doge bork. Much ruin diet long bois long doggo extremely
+            cuuuuuute, ur givin me a spook.
+          </p>
+        </div>
+      </div>
+    </div>
+  </>
+);
 
 export default About;
